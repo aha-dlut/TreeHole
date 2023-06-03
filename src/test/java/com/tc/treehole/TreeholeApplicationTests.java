7966346350b9820d4d3e68a17cfc2d4fdea9c8c9
15855021058 <1292291248@proton.me>
2023-06-03 20:21:40 +0800

    init repo

---
+++ src/test/java/com/tc/treehole/TreeholeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tc.treehole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreeholeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
