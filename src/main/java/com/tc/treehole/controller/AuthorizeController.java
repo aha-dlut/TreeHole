8edf31dc3008044f4b26bfd74c5b2b0b34efb711
aha-dlut <1292291248@qq.com>
2023-06-10 13:40:59 +0800

    cookie

--- src/main/java/com/tc/treehole/controller/AuthorizeController.java
+++ src/main/java/com/tc/treehole/controller/AuthorizeController.java
@@ -5,13 +5,17 @@ import com.tc.treehole.dto.GIthubUser;
 import com.tc.treehole.mapper.UserMapper;
 import com.tc.treehole.model.User;
 import com.tc.treehole.provider.GithubProvider;
+import okhttp3.Response;
+import org.apache.http.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 /*
@@ -39,7 +43,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request
+                           HttpServletRequest request,
+                           HttpServletResponse response
                            ){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
        accessTokenDTO.setCode(code);
@@ -52,13 +57,17 @@ public class AuthorizeController {
 
         if(githubUser != null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtModified());
 
             userMapper.insert(user);
+            response.addCookie(new Cookie("token",token));
+            //登录成功，插入数据库，相当于写入session
+
 
             request.getSession().setAttribute("user",githubUser);
             return "redirect:/";
